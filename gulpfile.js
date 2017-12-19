409923f76eb5e92fdd30f3751a82ea4dfc8e6fd4
Gilson Terra <gilsonterra@gmail.com>
2017-12-19 16:14:48 -0200

    criando projeto

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp     = require('gulp');
+var minifier = require('gulp-minifier');
+var clean    = require('gulp-clean');
+var concat   = require('gulp-concat');
+var rename   = require('gulp-rename');
+
+gulp.task('scripts', function () {
+    return gulp.src([
+            'src/js/**'                    
+        ])
+        .pipe(concat('bundle.min.js'))
+        .pipe(minifier({
+            minify: true,
+            minifyJS: true,
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('css', function () {
+    return gulp.src([
+            'src/css/**'            
+        ])
+        .pipe(concat('style.min.css'))
+        .pipe(minifier({
+            minify: true,
+            minifyCSS: true,
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('index', function () {
+    return gulp.src([
+            'src/index-dev.html'           
+        ])        
+        .pipe(minifier({
+            minify: true,
+            minifyCSS: true,
+            collapseWhitespace: true
+        }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src('dist', {
+        read: false
+    }).pipe(clean());
+});
+
+gulp.task('default', ['index', 'scripts', 'css'], function () {
+
+});
